a3d0aa2d1c18ebc11f22857df6a6be46c84138f0
mdoerner4 <mdoerner38@gmail.com>
2014-06-08 14:20:41 -0400

    commit1
    plot1 added

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## read data
+datao <- read.table("household_power_consumption.txt", sep= ";", as.is = "Date", header=TRUE)
+datao$Date <- as.Date(datao$Date, format = "%d/%m/%Y")
+datao$Time <- strptime(datao$Time, format = "%H:%M:%S")
+datao$Global_active_power <- as.numeric(as.character(datao$Global_active_power))
+
+## new data with just 2007-02-01 and 2007-02-02
+date1 <- grep("2007-02-01", datao$Date)
+feb1 <- datao[date1,]
+date2 <- grep("2007-02-02", datao$Date)
+feb2 <- datao[date2,]
+datac <- rbind(feb1, feb2)
+
+## create histogram to png file
+png(file = "plot1.png")
+hist(datac$Global_active_power, 
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.off()
